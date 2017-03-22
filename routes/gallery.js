3abb14bff07d8797a32c9eec1356366da2ee3a33
jorditruji <jordimoreratrujillo@gmail.com>
2017-03-22 21:47:54 +0100

    commit
    fes el merge quan puguis

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var db = null // global variable to hold the connection
+
+MongoClient.connect('mongodb://localhost:27017/INSTADB', function(err,database) {
+    if(err) { console.error(err) }
+    db = database // once connected, assign the connection to the global variable
+})
+
 
 
 router.get('/', function(req, res, next) {
