80268ee85ded61d1717f12464bf6c354855717b0
jorditruji <jordimoreratrujillo@gmail.com>
2017-03-23 17:15:11 +0100

    commiting!!!!!

--- random.js
+++ random.js
@@ -3,22 +3,36 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var db = null // global variable to hold the connection
 var mongoose = require('mongoose')
-MongoClient.connect('mongodb://localhost:27017/INSTADB', function(err,database) {
-	if(err) { console.error(err) }
-    db = database // once connected, assign the connection to the global variable
-    console.log('connected_2');
-})
 
+ MongoClient.connect('mongodb://localhost:27017/INSTADB', function(err,database) {
+    if(err) { console.error(err) }
+       console.log('connected to mongodb 2')
+       db = database // once connected, assign the connection to the global variable
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //TODO:::::::
   //mongoose.model('rand').
+  var str_res = null
+  var filename = null;
   //agafar 1 fotos aleatoria i mostrarla amb descripció i nom d'usuari.
-  var filename=db.collection('InstaFotos').find({},{filename:1, _id:0}).limit( 1 ).skip( Math.random() * db.collection('InstaFotos').count() ).toString(function(err, filename) {
-    console.log(JSON.stringify(filename));
-});
+  db.collection('InstaFotos').find({},{filename:1, _id:0}).limit( 1 ).skip(Math.floor(Math.random() * 40 )).toArray(function(err, filename) {
+    console.log('------------------------------------');
+    console.log(filename);
+    console.log('+++++++');
 
-  res.render('random', filename);
+    str_res=JSON.stringify(filename[0].filename);
+    str_res=str_res.substring(35,113);
+    str_res = str_res.replace(/\\/g, "/");
+    str_res = str_res.replace("//", "/");
+    str_res = str_res.replace("//", "/");
+    str_res = str_res.replace("//", "/");
+    console.log(str_res);
+    //db.close();
+    res.render('random', {result: str_res});
+});
+  
+  
 });
 
 module.exports = router;
